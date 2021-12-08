d263fc784970c367ab57852131ebbf70fc064319
Daniela Massiceti <daniela.massiceti@gmail.com>
2021-12-08 18:23:45 +0000

    added option to override default number of workers for data loading

--- data/queues.py
+++ data/queues.py
@@ -2,52 +2,57 @@
 # Licensed under the MIT license.
 
 import torch
-import numpy as np
-from data.datasets import UserEpisodicORBITDataset, ObjectEpisodicORBITDataset
+from typing import Optional
 from data.samplers import TaskSampler
+from data.datasets import UserEpisodicORBITDataset, ObjectEpisodicORBITDataset
 
 class DatasetQueue:
     """
     Class for a queue of tasks sampled from UserEpisodicORIBTDataset/ObjectEpisodicORBITDataset.
 
     """
-    def __init__(self, tasks_per_user, shuffle,  test_mode):
+    def __init__(self, tasks_per_user, shuffle, test_mode, override_num_workers: Optional[int]=None):
         """
         Creates instance of DatasetQueue.
         :param tasks_per_user: (int) Number of tasks per user to add to the queue.
         :param shuffle: (bool) If True, shuffle tasks, else do not shuffled.
         :param test_mode: (bool) If True, only return target set for first task per user.
+        :param num_workers: (Optional[int]) Number of workers to use. Overrides defaults (4 if test, 8 otherwise).
         :return: Nothing.
         """
         self.tasks_per_user = tasks_per_user
         self.shuffle = shuffle
         self.test_mode = test_mode
-        self.num_workers = 4 if self.test_mode else 8
+        if override_num_workers is None:
+            self.num_workers = 4 if self.test_mode else 8
+        else:
+            self.num_workers = override_num_workers
+
         self.num_users = None
         self.collate_fn = self.squeeze
 
     def squeeze(self, batch):
         #assumes batch_size = 1
-        squeezed_batch = {} 
+        assert len(batch) == 1, "DataLoader needs a batch size of 1!"
+        squeezed_batch = {}
         for k,v in batch[0].items():
             if isinstance(v, torch.Tensor):
+                # We have a single tensor. Remove any axes of dimension 1.
                 squeezed_batch[k] = v.squeeze(0)
-            elif isinstance(v, list):
-                if all(isinstance(b, np.ndarray) for b in v):
-                    squeezed_batch[k] = v
-                elif all(isinstance(b, torch.Tensor) for b in v):
-                    squeezed_batch[k] = [b.squeeze(0) for b in v]
-            elif isinstance(v, np.ndarray):
+            elif isinstance(v, list) and all(isinstance(b, torch.Tensor) for b in v):
+                # If we have a list of tensors, return a list of squeezed tensors.
+                squeezed_batch[k] = [b.squeeze(0) for b in v]
+            else:
+                # Just use the value
                 squeezed_batch[k] = v
-
         return squeezed_batch
-    
+
     def get_num_users(self):
         return self.num_users
-    
+
     def get_cluster_classes(self):
         return self.dataset.cluster_classes
-    
+
     def get_tasks(self):
         return torch.utils.data.DataLoader(
                 dataset=self.dataset,
@@ -55,16 +60,16 @@ class DatasetQueue:
                 num_workers=self.num_workers,
                 sampler=TaskSampler(self.tasks_per_user, self.num_users, self.shuffle, self.test_mode),
                 collate_fn=self.collate_fn
-                ) 
-    
+                )
+
 class UserEpisodicDatasetQueue(DatasetQueue):
     def __init__(self, root, way_method, object_cap, shot_method, shots, video_types, subsample_factor, num_clips, clip_length, preload_clips, frame_size, tasks_per_user, test_mode, with_cluster_labels, with_caps, shuffle):
         DatasetQueue.__init__(self, tasks_per_user, shuffle, test_mode)
         self.dataset = UserEpisodicORBITDataset(root, way_method, object_cap, shot_method, shots, video_types, subsample_factor, num_clips, clip_length, preload_clips, frame_size, test_mode, with_cluster_labels, with_caps)
         self.num_users = self.dataset.num_users
-    
+
 class ObjectEpisodicDatasetQueue(DatasetQueue):
     def __init__(self, root, way_method, object_cap, shot_method, shots, video_types, subsample_factor, num_clips, clip_length, preload_clips, frame_size, tasks_per_user, test_mode, with_cluster_labels, with_caps, shuffle):
         DatasetQueue.__init__(self, tasks_per_user, shuffle, test_mode)
         self.dataset = ObjectEpisodicORBITDataset(root, way_method, object_cap, shot_method, shots, video_types, subsample_factor, num_clips, clip_length, preload_clips, frame_size, test_mode, with_cluster_labels, with_caps)
-        self.num_users = self.dataset.num_users    
+        self.num_users = self.dataset.num_users
