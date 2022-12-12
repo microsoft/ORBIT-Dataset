91c84ab8809c3d1053ec7169ac32f06a03ab3c8f
Daniela Massiceti <daniela.massiceti@gmail.com>
2022-12-12 05:22:26 +0000

    updated feature extractor output sizes

--- model/feature_extractors.py
+++ model/feature_extractors.py
@@ -50,18 +50,18 @@ def create_feature_extractor(feature_extractor_name: str, pretrained: bool, with
         pretrained_cfg=get_pretrained_cfg('vit_small_patch32_224_in21k')
         pretrained_cfg['url'] = 'https://storage.googleapis.com/vit_models/augreg/S_32-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0.npz'
         feature_extractor = vit_small_patch32_224_in21k(pretrained=pretrained, pretrained_cfg=pretrained_cfg, num_classes=0)
-        feature_extractor.output_size = 1280
+        feature_extractor.output_size = 384
     elif feature_extractor_name == 'vit_b_32':
         pretrained_cfg=get_pretrained_cfg('vit_base_patch32_224_in21k')
         pretrained_cfg['url'] = 'https://storage.googleapis.com/vit_models/augreg/B_32-i21k-300ep-lr_0.001-aug_medium1-wd_0.03-do_0.0-sd_0.0.npz'
         feature_extractor = vit_base_patch32_224_in21k(pretrained=pretrained, pretrained_cfg=pretrained_cfg, num_classes=0)
-        feature_extractor.output_size = 1280
+        feature_extractor.output_size = 768
     elif feature_extractor_name == 'vit_b_32_clip':
         pretrained_cfg=get_pretrained_cfg('vit_base_patch32_224_clip_laion2b')
         pretrained_cfg['hf_hub_id'] ='laion/CLIP-ViT-B-32-laion2B-s34B-b79K'
         pretrained_cfg['hf_hub_filename'] = 'open_clip_pytorch_model.bin'
         feature_extractor = vit_base_patch32_224_clip_laion2b(pretrained=pretrained, pretrained_cfg=pretrained_cfg, num_classes=0)
-        feature_extractor.output_size = 1280
+        feature_extractor.output_size = 768
     else:
         raise ValueError(f"Invalid feature_extractor_name: {feature_extractor_name}")
 
